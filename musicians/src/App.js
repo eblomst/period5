44cd36a5f3179afc6271528cb116f6367822f22c
eblomst <emma.blomsterberg@hotmail.com>
2017-05-10 17:17:01 +0200

    Exercise 1.1 done

--- musicians/src/App.js
+++ musicians/src/App.js
@@ -2,20 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class TableRender extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      musicians: this.props.data,
+      filterVal: 0
+    }
+  }
+
+  onFilterChange =(evt) => {
+    this.setState({filterVal: evt.target.value});
+  }
   render() {
+    let data = this.state.musicians;
+    const propRows = Object.keys(data[0]).map(function(prop) {
+      return (<th key={prop}>{prop}</th>)
+    })
+    const dataRows = 
+      data.filter(musician=> musician.stars >= this.state.filterVal).map((musician) =>
+        <tr key={musician.id}>
+                  <td>{musician.id}</td>
+                  <td>{musician.name}</td>
+                  <td>{musician.stars}</td>
+                </tr>
+      );
+    
+  
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Table">
+      Show stars above: <input onChange={this.onFilterChange}/>
+        <table>
+          <thead>
+            <tr>{propRows}</tr>
+          </thead>
+          <tbody>
+          {dataRows}
+          </tbody>
+        </table>
       </div>
     );
   }
 }
 
-export default App;
+export default TableRender;
+
+
